3964c3afe7404f8f4848ea71ba38062ff15bd742
Надя <nadya.ptz@gmail.com>
2024-08-23 19:43:46 +0300

    initial

--- Module_6_1.py
+++ Module_6_1.py
@@ -1,1 +1,50 @@
-print('hello')
+class Animal:
+    def __init__(self, name, alive=True, fed=False):
+        self.alive = alive
+        self.fed = fed
+        self.name = name
+    def eat(self, food):
+        if food.edable == True:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+class Plant:
+    def __init__(self, name, edable=False):
+        self.edable = edable
+        self.name = name
+
+class Mammal(Animal):
+    pass
+
+
+class Predator(Animal):
+    pass
+
+class Flower(Plant):
+    pass
+
+class Fruit(Plant):
+    def __init__(self, name, edable=False):
+        self.edable=True
+        self.name=name
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name) # Волк
+print(p1.name) # Цветик семицветик
+print(a2.name) # Хатико
+print(p2.name) # апельсин
+
+print(a1.alive) # волк живой True
+print(a2.fed)   # Хатико сытый - False
+a1.eat(p1)      # волк ест цветик
+a2.eat(p2)      # хатико ест апельсин
+print(a1.alive) # волк умер
+print(a2.fed)   # хатико должен быть сыт
